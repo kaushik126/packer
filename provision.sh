b4667f377d4b983b5b34e3c6d2153b6e5b4ce0de
root <root@ip-10-0-1-30.ec2.internal>
2018-03-13 07:15:43 +0000

    packer updated

--- provision.sh
+++ provision.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-
-
-install java-1.8.0 -y
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven$
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
-
-wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
-sudo mkdir /opt/gradle
-sudo unzip -d /opt/gradle gradle-3.4.1-bin.zip
-export PATH=$PATH:/opt/gradle/gradle-3.4.1/bin
-
-
-
-
-
-
-sudo yum upgrade -y
-sudo yum install git -y
+sudo yum install httpd apache2 -y
+sudo yum update -y
+set -e
+sudo touch /var/www/html/index.html
+echo "Hello ?(0_0)? world" | sudo tee /var/www/html/index.html
+sudo service httpd start
+sudo chkconfig httpd on
